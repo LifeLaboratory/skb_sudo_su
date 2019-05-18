e96a2c173342a42a9d85e4dd657589382864a56e
alexandravg <36479153+alexandravg@users.noreply.github.com>
2019-05-18 21:40:37 +0700

    Search added

--- android/app/src/main/java/ru/lifelaboratory/skb/SearchActivity.java
+++ android/app/src/main/java/ru/lifelaboratory/skb/SearchActivity.java
@@ -5,13 +5,30 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import ru.lifelaboratory.skb.REST.Item;
+import ru.lifelaboratory.skb.REST.User;
 
 public class SearchActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    private TextView searchView;
+    private MainListAdapter mainListAdapter = null;
+    private ListView mainList = null;
+    private ArrayList<ru.lifelaboratory.skb.Entity.Item> items;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -44,9 +61,37 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        searchView = (EditText) findViewById(R.id.search);
+        items = new ArrayList<>();
+        mainList = (ListView) findViewById(R.id.lvsearch);
+        mainListAdapter = new MainListAdapter(SearchActivity.this, items);
+        mainList.setAdapter(mainListAdapter);
+        searchView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) { // Enter
+                    String type = "name";
+                    String whatToSearch = searchView.getText().toString();
+                    Item search = MainActivity.server.create(Item.class);
+                    search.search(type, whatToSearch).enqueue(new Callback<List<ru.lifelaboratory.skb.Entity.Item>>() {
+                        @Override
+                        public void onResponse(Call<List<ru.lifelaboratory.skb.Entity.Item>> call, Response<List<ru.lifelaboratory.skb.Entity.Item>> response) {
+                            items.addAll(response.body());
+                            mainListAdapter.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void onFailure(Call<List<ru.lifelaboratory.skb.Entity.Item>> call, Throwable t) {
+                            Toast.makeText(getApplicationContext(), "Ошибка соединения с сервером", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                return false;
+            }
+        });
     }
 
 }
