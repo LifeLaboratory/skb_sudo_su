1268eae89020d12621d5fa677cd650c4c166f274
Denis Soldatov <moralarch@gmail.com>
2019-05-18 20:50:12 +0700

    test

---
+++ app/api/helpers/add_test.py
@@ -0,0 +1,11 @@
+from app.api.src.matrix import *
+
+
+with open("presentation.txt","r") as f:
+    lines = f.readlines()
+
+
+for i in lines:
+    j = i.split("/")
+    code = j[len(j) - 1].split(".")[0]
+    get_data(code)
