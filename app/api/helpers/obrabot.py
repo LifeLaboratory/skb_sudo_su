5dedc60766831fedb7aee2be49e10c197dc45b28
Denis Soldatov <moralarch@gmail.com>
2019-05-19 02:38:53 +0700

    obrabot

---
+++ app/api/helpers/obrabot.py
@@ -0,0 +1,31 @@
+from app.api.base.base_sql import Sql
+
+
+
+
+
+
+def get_analit:
+
+    sql = """select category, "процент_всего"
+    from (
+    select *
+     , ("просрочка"::float / case when "всего" > 0 then "всего" else 1 end::float)::float "процент_всего"
+    from (
+    select 
+        category
+      , count(1) filter(where expired) as "просрочка"
+      , count(1) as "всего"
+      , count(1) filter(where close) as "количество закрытых"
+    from user_nom
+      left join nomenclature using(id_nom)
+    where id_user = 9
+    group by category
+    ) n
+    ) n
+    order by "процент_всего" desc, category"""
+    data = Sql.exec(sql)
+    return data
+
+
+
