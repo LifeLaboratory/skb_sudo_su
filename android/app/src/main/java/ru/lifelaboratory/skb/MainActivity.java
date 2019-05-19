e7e6cd323fb419ce08a4ed2bda199eccd5b981ea
Boris Bochkarev <Boris-Bochkaryov@yandex.ru>
2019-05-19 11:13:48 +0700

    Запрос на доступ к камере

--- android/app/src/main/java/ru/lifelaboratory/skb/MainActivity.java
+++ android/app/src/main/java/ru/lifelaboratory/skb/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.lifelaboratory.skb;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.Dialog;
 import android.app.NotificationChannel;
@@ -8,9 +9,12 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -75,6 +79,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (permissionStatus != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.INTERNET}, 0);
+        }
+        
         server = new Retrofit.Builder()
                 .baseUrl(Constants.SERVER)
                 .addConverterFactory(GsonConverterFactory.create())
